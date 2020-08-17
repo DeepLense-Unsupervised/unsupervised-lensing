1d2f9ac4c65db6858b03a192eae27882e8cd188b
Pranath Reddy <pranath.mail@gmail.com>
2020-08-17 17:47:59 +0530

    added EMD

---
+++ unsupervised_lensing/utils/EMD_Lensing.py
@@ -0,0 +1,28 @@
+import numpy as np
+import ot
+from scipy.spatial import distance
+
+def EMD(data_path='./Data/no_sub_test.npy', recon_path='./Results/Recon_samples.npy'):
+
+    data = np.load(data_path, allow_pickle='True')
+    recon_data = np.load(recon_path, allow_pickle='True')
+
+    data_temp = []
+    for i in range(data.shape[0]):
+        for j in range(data[i].shape[0]):
+            data_temp.append(data[i][j])
+    data = np.asarray(data_temp)
+
+    print(data.shape)
+    print(recon_data.shape)
+
+    EMD = 0
+    for i in range(data.shape[0]):
+        
+        data_1D = np.sum(data[i][0], axis=0)
+        recon_1D = np.sum(recon_data[i][0], axis=0)
+        cost_matrix = distance.cdist(data_1D.reshape(-1,1), recon_1D.reshape(-1,1), 'euclidean')
+        EMD += ot.emd2([],[],cost_matrix)
+        
+    return EMD/data.shape[0]
+
