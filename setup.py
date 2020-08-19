75aa2b1a694d2bb157d596300e7c4ef2b71dc4a5
Pranath Reddy <f20160572@hyderabad.bits-pilani.ac.in>
2020-08-20 01:00:59 +0530

    added setup files

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+setup(
+  name = 'unsupervised_lensing',
+  packages = ['unsupervised_lensing'],
+  version = '0.1',
+  license='MIT',
+  description = 'A PyTorch-based tool for Unsupervised Deep Learning applications in strong lensing cosmology',
+  author = 'K Pranath Reddy',
+  author_email = 'pranath.mail@gmail.com',
+  url = 'https://github.com/DeepLense-Unsupervised/unsupervised-lensing',
+  keywords = ['Gravitational Lensing', 'Unsupervised Deep Learning', 'Dark Matter'],
+  install_requires=[            
+          'numpy==1.18.5',
+          'cython==0.29.21',
+          'scipy==1.4.1',
+          'POT==0.7.0',
+          'tqdm==4.47.0',
+          'matplotlib==3.2.2',
+          'seaborn==0.10.1',
+          'torch==1.6.0',
+          'torchvision==0.7.0',
+          'googledrivedownloader==0.4.0',
+      ],
+  classifiers=[
+    'Development Status :: 4 - Beta',
+    'License :: OSI Approved :: MIT License',
+  ],
+)
